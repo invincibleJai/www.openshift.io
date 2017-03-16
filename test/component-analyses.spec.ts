2c50e1aba52442401141c713bbfa3e651ae31201
jaivardhan <mailjai.vardhan@gmail.com>
2017-03-16 16:27:39 +0530

    setup for testcases

---
+++ test/component-analyses.spec.ts
@@ -0,0 +1,13 @@
+import {ComponentAnalyses} from '../app/component-analyses';
+
+describe('ComponentAnalyses Test', () => {
+    let componentAnalyses = new ComponentAnalyses();
+    //componentAnalyses.buildComponentAnalyses();
+    it('Should be true', () => {
+        expect(1 + 1).toBe(2);
+    })
+
+    it('Should be true', () => {
+        expect(2*2).toBe(4);
+    })
+});
---
+++ test/index.spec.ts
@@ -0,0 +1,11 @@
+
+import * as $ from 'jquery';
+describe('Test', () => {
+    it('Should be true', () => {
+        expect(1 + 1).toBe(2);
+    })
+
+    it('Should be true', () => {
+        expect(2*2).toBe(4);
+    })
+});
